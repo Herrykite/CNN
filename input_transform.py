1e658b19c85ef675af60083391e037066c90e266
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-10-12 20:26:18 +0800

    Add files via upload

--- input_transform.py
+++ input_transform.py
@@ -9,19 +9,20 @@ from torchvision import transforms
 transform = transforms.Compose([
     transforms.Resize(240),  # 缩放图片，保持长宽比不变，最短边的长为240像素,
     transforms.ToTensor(),  # 将图片转换为Tensor,归一化至[0,1]
-    transforms.Normalize(mean=[0.5], std=[0.5])  # 标准化至[-1,1]
+    transforms.Normalize(mean=0.5, std=0.5)  # 标准化至[-1,1]
 ])
 
 
 class DataSet(data.Dataset):
-    def __init__(self, root):
+    def __init__(self, root, num):
         # 所有图片的绝对路径
         images = os.listdir(root)
         self.images = [os.path.join(root, k) for k in images]
         self.transforms = transform
+        self.num = num
 
-    def __getitem__(self, index):
-        img_path = self.images[index]
+    def __getitem__(self, images):
+        img_path = self.images[self.num]
         figure = Image.open(img_path).convert('L')
         if self.transforms:
             img_data = self.transforms(figure)
