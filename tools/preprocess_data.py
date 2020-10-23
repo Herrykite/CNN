e84046cc1c06a1bfe38112c67d1b1309c9da894c
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-10-23 16:02:27 +0800

    Add files via upload

--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -6,7 +6,7 @@ from ConvNet.tools.deal_with_obj import loadObj
 from ConvNet.config.defaults import get_cfg_defaults
 
 cfg = get_cfg_defaults()
-path = cfg.OUTPUT.CNN_INITIAL_DATA_PATH
+path = cfg.OUTPUT.CNN_INITIAL_DATA_PATH_MINI
 
 
 def mkdir(creat_path):
@@ -20,8 +20,9 @@ def mkdir(creat_path):
 
 def Woodblock(order, data):  # 活字印刷术
     listdir = os.listdir('../')
-    if cfg.OUTPUT.CNN_FORMAT_JSON_FILENAM in listdir:
-        f = open('./' + cfg.OUTPUT.CNN_FORMAT_JSON_FILENAM)
+    listdir.sort(key=lambda obj: len(obj))
+    if cfg.OUTPUT.CNN_FORMAT_JSON_FILENAME in listdir:
+        f = open('../' + cfg.OUTPUT.CNN_FORMAT_JSON_FILENAME)
         model = json.load(f)
         f.close()
         for j in range(3):
@@ -36,8 +37,9 @@ def Woodblock(order, data):  # 活字印刷术
 
 
 def initial_load():
-    input_path = cfg.INPUT.VERTICS_PATH
+    input_path = cfg.INPUT.VERTICS_PATH_MINI
     file_list = os.listdir(input_path)
+    file_list.sort(key=lambda x: len(x))
     for count in range(len(file_list)):
         datas = []
         if file_list[count].endswith('.obj'):
@@ -49,11 +51,12 @@ def initial_load():
 def get_vertics(count):
     f = open(path + '/' + re.sub("\D", "", str(count)) + '.data')
     model = json.load(f)
-    vertics = []
-    for i in range(len(model['camera2']['mesh'][0]['points'])):
-        vertics.append(model['camera2']['mesh'][0]['points'][i])
-    return np.array(vertics, dtype=np.float32)
+    f.close()
+    vertics = np.array(model['camera2']['mesh'][0]['points'], dtype=np.float32)
+    faces = model['camera2']['mesh'][1]['points']
+    return vertics, faces
 
 
 if __name__ == '__main__':
+    mkdir(cfg.OUTPUT.CNN_INITIAL_DATA_PATH_MINI)
     initial_load()
