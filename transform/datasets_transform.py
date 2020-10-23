653855d27fd52fef61a292713ed7b581cae80cb3
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-10-23 16:14:52 +0800

    Add files via upload

--- transform/datasets_transform.py
+++ transform/datasets_transform.py
@@ -11,11 +11,12 @@ from ConvNet.tools.deal_with_obj import writeObj
 cfg = get_cfg_defaults()
 transform = transforms.Compose([
     transforms.Resize(cfg.DATASETS.TRANSFORM_RESIZE),
-    # 缩放图片，保持长宽比不变，最短边的长为240像素,
+    # transforms.ColorJitter(brightness=(0.5, 1.5), contrast=(0.5, 1.5), saturation=(0.5, 1.5), hue=(-0.25, 0.25)),
+    # transforms.RandomRotation(5),
+    # transforms.RandomAffine(degrees=0, translate=(0.05, 0.05), scale=(0.95, 1.05), shear=(-5, 5, -5, 5), fillcolor=0),
     transforms.ToTensor(),
-    # 将图片转换为Tensor,归一化至[0,1]
+    # transforms.RandomErasing(p=1, scale=(0.001, 0.01), ratio=(0.5, 2.0), value=0),
     transforms.Normalize(mean=0.5, std=0.5)
-    # 标准化至[-1,1]
 ])
 
 restore = transforms.Compose([
