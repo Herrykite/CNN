3c8c935223614187eeef626a1e06969875c02d76
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-10-28 15:59:21 +0800

    Add files via upload

---
+++ tools/resize_image.py
@@ -0,0 +1,20 @@
+import os
+from ConvNet.config.defaults import get_cfg_defaults
+from PIL import Image
+from torchvision import transforms
+from ConvNet.tools.preprocess_data import mkdir
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+cfg = get_cfg_defaults()
+transform = transforms.Compose([
+    transforms.Resize(cfg.DATASETS.TRANSFORM_RESIZE),
+])
+
+image_path = cfg.INPUT.IMAGES_PATH
+save_image_path = cfg.DATASETS.SAVE_RESIZE_IMAGES
+images = os.listdir(image_path)
+mkdir(save_image_path)
+for i in range(len(images)):
+    figure = Image.open(image_path + images[i]).convert('L')
+    new_figure = transform(figure)
+    new_figure.save(save_image_path + images[i])
