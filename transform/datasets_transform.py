f0f54f5fc5ad1cd880f17ca33d0d5b15f85effec
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-10-30 17:12:20 +0800

    Add files via upload

--- transform/datasets_transform.py
+++ transform/datasets_transform.py
@@ -1,3 +1,4 @@
+import math
 import os
 import torch
 from torch.utils import data
@@ -5,21 +6,21 @@ from PIL import Image
 import numpy as np
 from torchvision import transforms
 from ConvNet.config.defaults import get_cfg_defaults
-from ConvNet.tools.preprocess_data import get_vertics
+from ConvNet.tools.preprocess_data import get_vertics, mkdir
 from ConvNet.tools.deal_with_obj import writeObj
 
 cfg = get_cfg_defaults()
 transform = transforms.Compose([
-    transforms.ColorJitter(brightness=(0.5, 1.5), contrast=(0.5, 1.5), saturation=(0.5, 1.5), hue=(-0.25, 0.25)),
+    transforms.ColorJitter(brightness=(0.5, 1.5), contrast=(0.8, 1.2), saturation=(0.8, 1.2), hue=(-0.2, 0.2)),
     transforms.RandomRotation(5),
-    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05), scale=(0.95, 1.05), shear=(-5, 5, -5, 5), fillcolor=0),
+    transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), scale=(0.95, 1.05), shear=(-5, 5, -5, 5)),
     transforms.ToTensor(),
     transforms.RandomErasing(p=1, scale=(0.001, 0.01), ratio=(0.5, 2.0), value=0),
-    transforms.Normalize(mean=0.5, std=0.5)
+    transforms.Normalize(mean=0.3156022930958101, std=0.28214540372352737)
 ])
 
 restore = transforms.Compose([
-    transforms.Normalize(mean=-1, std=2),
+    transforms.Normalize(mean=-1.1185803097649145, std=3.5442718073830264),
     transforms.ToPILImage()
 ])
 
@@ -35,14 +36,16 @@ class DataSet(data.Dataset):
     def __getitem__(self, item):
         img_path = self.images[item]
         figure = Image.open(img_path).convert('L')
-        label = get_vertics(item)
+        label, faces = get_vertics(item)
         if self.transforms:
             img_data = self.transforms(figure)
         else:
             figure = np.asarray(figure)
             img_data = torch.from_numpy(figure)
         lab_data = torch.from_numpy(label)
+        # 下两行用于训练前测试标签与输入是否相互对应，正式训练时注释
         # check_before_train(img_data, lab_data, faces, item)
+        # restore(img_data).show()
         return img_data, lab_data
 
     def __len__(self):
@@ -53,8 +56,23 @@ def check_before_train(images, labels, faces, item):
     restore(images).convert('L')
     vertics = []
     for i in range(len(labels) // 3):
-        vertics.append([labels[i], labels[i + len(labels) // 3], labels[i + 2*len(labels) // 3]])
+        vertics.append([labels[i], labels[i + len(labels) // 3], labels[i + 2 * len(labels) // 3]])
+    mkdir(cfg.INPUT.CHECK)
     writeObj(cfg.INPUT.CHECK + str(item) + '_check.obj', vertics, faces)
+
+
+def get_mean_std():
+    image_list = os.listdir(cfg.DATASETS.SAVE_RESIZE_IMAGES)
+    means, variances = 0, 0
+    for i in range(len(image_list)):
+        img_data = transform(Image.open(cfg.DATASETS.SAVE_RESIZE_IMAGES + image_list[i]).convert('L'))
+        means += img_data.mean()
+        variances += img_data.var()
+    mean = np.asarray(means) / len(image_list)
+    variance = np.asarray(variances) / len(image_list)
+    std = math.sqrt(variance)
+    print('mean= ', mean, 'std= ', std)
+    return mean, std
 
 
 class SingleTest:
