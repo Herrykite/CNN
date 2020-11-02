02bc102b1cb6c7a81893d360d219ad1930036c6f
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-11-02 17:40:14 +0800

    Add files via upload

--- jotting/notes.py
+++ jotting/notes.py
@@ -4,7 +4,9 @@ import torchvision
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from ConvNet.config.defaults import get_cfg_defaults
-from ConvNet.modeling.cnn import CNN
+from ConvNet.modeling.newcnn import CNN
+from ConvNet.transform.datasets_transform import DataSet
+
 # if np.any(np.isnan(input.cpu().numpy())):   # 判断输入数据是否存在nan
 #     print('Input data has NaN!')
 #
@@ -35,17 +37,18 @@ def tell_vertics_combine(vertics_x, vertics_y, vertics_z):
 
 if __name__ == '__main__':
     cfg = get_cfg_defaults()
-    # image_path = cfg.DATASETS.SAVE_RESIZE_IMAGES
-    # net = CNN()
-    # optimizer = torch.optim.Adam(net.parameters(), lr=cfg.SOLVER.BASE_LR)
-    # net.load_state_dict(torch.load(cfg.OUTPUT.PARAMETER + cfg.OUTPUT.SAVE_NET_FILENAME))
-    # print('loaded net successfully!')
-    # optimizer.load_state_dict(torch.load(cfg.OUTPUT.PARAMETER + cfg.OUTPUT.SAVE_OPTIMIZER_FILENAME))
-    # print('loaded optimizer successfully!')
-    # loader = DataLoader(DataSet(image_path), batch_size=cfg.INPUT.BATCH_SIZE, shuffle=True)
-    # for i, (images, labels) in enumerate(loader, start=1):
-    #     writer = SummaryWriter('./')
-    #     grid = torchvision.utils.make_grid(images)
-    #     writer.add_image('Image', grid, 0)
-    #     writer.add_graph(net, images)
-    #     writer.close()
+    image_path = cfg.DATASETS.SAVE_RESIZE_IMAGES
+    net = CNN()
+    optimizer = torch.optim.Adam(net.parameters(), lr=cfg.SOLVER.BASE_LR)
+    net.load_state_dict(torch.load(cfg.OUTPUT.PARAMETER + cfg.OUTPUT.SAVE_NET_FILENAME))
+    print('loaded net successfully!')
+    optimizer.load_state_dict(torch.load(cfg.OUTPUT.PARAMETER + cfg.OUTPUT.SAVE_OPTIMIZER_FILENAME))
+    print('loaded optimizer successfully!')
+    loader = DataLoader(DataSet(image_path), batch_size=cfg.INPUT.BATCH_SIZE, shuffle=True)
+    for i, (images, labels) in enumerate(loader, start=1):
+        writer = SummaryWriter('./')
+        grid = torchvision.utils.make_grid(images)
+        writer.add_image('Image', grid, 0)
+        writer.add_graph(net, images)
+        writer.close()
+    # tensorboard --logdir=./
