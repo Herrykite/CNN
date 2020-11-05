4ae68739e2996b17cbb4c74cb73e5c2125302781
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-11-05 17:45:26 +0800

    Add files via upload

--- jotting/notes.py
+++ jotting/notes.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from ConvNet.config.defaults import get_cfg_defaults
 from ConvNet.modeling.newcnn import CNN
 from ConvNet.transform.datasets_transform import DataSet
+from ConvNet.engine.trainer import proofread
 
 # if np.any(np.isnan(input.cpu().numpy())):   # 判断输入数据是否存在nan
 #     print('Input data has NaN!')
@@ -37,7 +38,7 @@ def tell_vertics_combine(vertics_x, vertics_y, vertics_z):
 
 if __name__ == '__main__':
     cfg = get_cfg_defaults()
-    image_path = cfg.DATASETS.SAVE_RESIZE_IMAGES
+    image_path = cfg.INPUT.SAVE_RESIZE_IMAGES
     net = CNN()
     optimizer = torch.optim.Adam(net.parameters(), lr=cfg.SOLVER.BASE_LR)
     net.load_state_dict(torch.load(cfg.OUTPUT.PARAMETER + cfg.OUTPUT.SAVE_NET_FILENAME))
@@ -46,9 +47,35 @@ if __name__ == '__main__':
     print('loaded optimizer successfully!')
     loader = DataLoader(DataSet(image_path), batch_size=cfg.INPUT.BATCH_SIZE, shuffle=True)
     for i, (images, labels) in enumerate(loader, start=1):
-        writer = SummaryWriter('./')
+        tb = SummaryWriter('./')
         grid = torchvision.utils.make_grid(images)
-        writer.add_image('Image', grid, 0)
-        writer.add_graph(net, images)
-        writer.close()
+        tb.add_image('Image', grid, 0)
+        tb.add_graph(net, images)
+        # tb.add_scalar('Loss', loss.item(), i)
+        tb.add_histogram('conv1.weight', net.conv_net.conv1.weight)
+        tb.add_histogram('bn2.weight', net.conv_net.bn2.weight)
+        tb.add_histogram('bn2.bias', net.conv_net.bn2.bias)
+        tb.add_histogram('conv3.weight', net.conv_net.conv3.weight)
+        tb.add_histogram('conv3.bias', net.conv_net.conv3.bias)
+        tb.add_histogram('bn4.weight', net.conv_net.bn4.weight)
+        tb.add_histogram('bn4.bias', net.conv_net.bn4.bias)
+        tb.add_histogram('conv5.weight', net.conv_net.conv5.weight)
+        tb.add_histogram('conv5.bias', net.conv_net.conv5.bias)
+        tb.add_histogram('bn6.weight', net.conv_net.bn6.weight)
+        tb.add_histogram('bn6.bias', net.conv_net.bn6.bias)
+        tb.add_histogram('conv7.weight', net.conv_net.conv7.weight)
+        tb.add_histogram('conv7.bias', net.conv_net.conv7.bias)
+        tb.add_histogram('bn8.weight', net.conv_net.bn8.weight)
+        tb.add_histogram('bn8.bias', net.conv_net.bn8.bias)
+        tb.add_histogram('conv9.weight', net.conv_net.conv9.weight)
+        tb.add_histogram('conv9.bias', net.conv_net.conv9.bias)
+        tb.add_histogram('bn10.weight', net.conv_net.bn10.weight)
+        tb.add_histogram('bn10.bias', net.conv_net.bn10.bias)
+        tb.add_histogram('conv11.weight', net.conv_net.conv11.weight)
+        tb.add_histogram('conv11.bias', net.conv_net.conv11.bias)
+        tb.add_histogram('bn12.weight', net.conv_net.bn12.weight)
+        tb.add_histogram('bn12.bias', net.conv_net.bn12.bias)
+        tb.add_histogram('fc.weight', net.fc.fc.weight)
+        tb.add_histogram('fc.bias', net.fc.fc.bias)
+        tb.close()
     # tensorboard --logdir=./
