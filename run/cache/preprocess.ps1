f1567852a925018b40bdab1d9c71d63523e9cce2
Herrykite <68586886+Herrykite@users.noreply.github.com>
2020-12-18 17:40:59 +0800

    Add files via upload

--- run/cache/preprocess.ps1
+++ run/cache/preprocess.ps1
@@ -1,3 +1,5 @@
+cd 'D:\Program Files\JetBrains\Python Project\ConvNet\config'
+python user_input.py
 cd 'D:\Program Files\JetBrains\Python Project\ConvNet\tools'
 python preprocess_data.py
 python resize_image.py
